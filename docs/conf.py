6401167f19b2b35ea6b679e0fd503432d74c6653
Florian Matter <florianmatter@gmail.com>
2022-04-05 08:02:32 +0200

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src/pyradigms"))
+sys.path.insert(0, os.path.abspath("../src"))
 
 
 # -- Project information -----------------------------------------------------
