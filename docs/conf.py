1cc13502d21615e56e6805b709ebcd74e1b87d1c
Florian Matter <florianmatter@gmail.com>
2022-04-05 08:21:23 +0200

    conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,8 @@ autodoc_mock_imports = [
     'pandas',
     "numpy",
     "clldutils",
-    "attrs.config"
+    "attrs.config",
+    "attrs.Factory"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
