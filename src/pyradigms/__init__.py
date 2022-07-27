b9ed402afd91e8990389ea09221ff5a5043a19ce
Florian Matter <florianmatter@gmail.com>
2022-07-26 18:35:59 -0700

    bump

--- src/pyradigms/__init__.py
+++ src/pyradigms/__init__.py
@@ -15,7 +15,7 @@ from attrs import define
 
 __author__ = "Florian Matter"
 __email__ = "florianmatter@gmail.com"
-__version__ = "0.0.5"
+__version__ = "0.0.6.dev"
 
 
 handler = colorlog.StreamHandler(None)
